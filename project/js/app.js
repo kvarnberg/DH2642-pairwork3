af7473ced9ba1d21dfa348b40e7fcad61abcb6fb
Josephine Kvarnberg <jkv@kth.se>
2020-02-13 11:36:32 +0100

    TW3.3

--- project/js/app.js
+++ project/js/app.js
@@ -16,10 +16,18 @@ function show(section){
 	let sec = sections.filter(s=> s !== section);
 	document.body.querySelector("#"+sec[0]).classList.add("hide")
 	document.body.querySelector("#"+section).classList.remove("hide")
+	window.location.hash = "#"+section
 }
+
+if (!window.location.hash){
+	window.location.hash = "#search";
+}
+
+window.onhashchange = () => show(window.location.hash.substring(1))
 
 const summaryNav=[()=> show("summary"), "Summary"];
 const backToSearch=[()=> show("search"), "Back to search"];
 
 new SearchController(model, document.body.querySelector("#search"), summaryNav)
 new SummaryController(model, document.body.querySelector("#summary"), backToSearch);
+
